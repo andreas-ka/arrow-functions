e2adf66ac25822742bb50e145d947401d949459c
Andreas Karlsson <andreas.krln@hotmail.com>
2023-05-08 07:54:16 +0000

    Session done

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,33 @@
 
 // Vanilla JavaScript Function
 
+/*function addTwoNumbers(a, b) {
+    return a + b;
+}
+sum = addTwoNumbers(2,3);
+console.log(sum)*/
+
 // Arrow Function With Parameters
-
+const addTwoNumbers = (a, b) => {
+    return a+ b;
+}
+sum = addTwoNumbers(3,7)
+console.log(sum)
 // Single Line Arrow Function With Parameters
-
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers2(3, 4)
+console.log(sum2)
 // Implicit Returns
+const saySomethting = message => console.log(message);
+saySomethting('Hello there!')
+
+const sayHello = () => console.log('hello');
+sayHello();
 
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    `<p>
+        This is a multiple line
+    </p>`
+)
+console.log(returnMultipleLines());
